6b47d53251c179c4ebf497d60eebccf12d078f11
RandomCatDude <kud.stanislav@gmail.com>
2024-12-16 07:22:52 +0500

    meow

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-mods.on_all_mods_loaded(function() for _, m in pairs(mods) do if type(m) == "table" and m.RoRR_Modding_Toolkit then for _, c in ipairs(m.Classes) do if m[c] then _G[c] = m[c] end end end end end)
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto(true)
 
-function __initialize()
+Initialize(function()
 	local worm = Monster_Card.find("ror", "magmaWorm")
 	List.wrap(worm.elite_list):add(gm.elite_type_find("blighted"))
-end
+end)
